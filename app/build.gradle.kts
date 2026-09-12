0e7e5b67be4483fd603063c9ecafdb98a93a28c9
Aditya <adityadav9532@gmail.com>
2026-09-12 08:52:12 +0530

    chore: Bump version to 1.2.6 (versionCode 156)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "echo.music.iad1tya"
         minSdk = 26
         targetSdk = 36
-        versionCode = 155
-        versionName = "1.2.5"
+        versionCode = 156
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
